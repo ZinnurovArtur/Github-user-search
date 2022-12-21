7a4748f3ff4c8495e19c0f76e40cf13f186b72e1
Artur <zinnurov_2012@mail.ru>
2022-12-21 18:47:50 +0000

    Added test image and grid

--- components/Card.js
+++ components/Card.js
@@ -1,22 +1,37 @@
-import React from 'react'
-import styles from "../styles/card.module.scss"
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/card.module.scss";
 const Card = () => {
   return (
     <div className={styles.card_container} id="maincard">
       <div className={styles.card}>
-        <div className={styles.card_body}>
-        <div className={styles.title_date_wrap}>
-        <h2 className={styles.card_title}>The Octocat</h2>
-        <h4>22/12/1998</h4>
+        <div className={styles.image_wrapper}>
+          <Image
+            src="/img/download.png"
+            alt="octocat"
+            width={111}
+            height={333}
+            className={styles.image_cover}
+          />
+        </div>
+        <div className={styles.main_data}>
+        <div className={styles.main_top}>
+          <h2 className={styles.card_title}>The Octocat</h2>
+          <h4>22/12/1998</h4>
+          <h3>@loh</h3>
+        </div>
+        <p className={styles.card_p}>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+            odio. Quisque volutpat mattis eros.
+          </p>
 
         </div>
-          <p className={styles.card_p}>
-          
-          </p>
-          </div>
-          </div>
-          </div>
-  )
-}
 
-export default Card
+
+      </div>
+      
+    </div>
+  );
+};
+
+export default Card;
