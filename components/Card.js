0ae499cb43a6a93f31158933ea21eab9588f1659
Artur <zinnurov_2012@mail.ru>
2022-12-21 11:53:44 +0000

    Added initial card

---
+++ components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div class="card-container" id="maincard">
+      <div class="card">
+        <div class="card-body">
+          <h2 class="card-title">How did we do?</h2>
+          <p class="card-p">
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
+          </p>
+          </div>
+          </div>
+          </div>
+  )
+}
+
+export default Card
