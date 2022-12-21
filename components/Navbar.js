e2345b303ac6f50bf4d1519a6458c92a3cb432d5
Artur <zinnurov_2012@mail.ru>
2022-12-21 11:53:54 +0000

    Added initial navbar

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+
+const Navbar =() => {
+  return (
+    <div className='navbar'>
+        <div className='title-nav'><h2>DevSearch</h2></div>
+        <div className='theme-change'>Light</div>
+    </div>
+  )
+}
+
+export default Navbar
