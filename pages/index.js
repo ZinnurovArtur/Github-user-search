f845a03e5b2b5aa924d0199529c0ea9fd23c1c2e
Artur <zinnurov_2012@mail.ru>
2022-12-21 11:54:19 +0000

    Main file change

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/Navbar'
+import Card from '../components/Card'
 import { Inter } from '@next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,7 +16,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div>loh</div>
+      <Navbar/>
+      <Card/>
       </main>
     </>
   )
