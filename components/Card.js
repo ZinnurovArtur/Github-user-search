15e7198d8f9ddeda074cb7b68827896190a03304
Artur <zinnurov_2012@mail.ru>
2022-12-23 20:09:16 +0000

    More card styling and adding card mored data

--- components/Card.js
+++ components/Card.js
@@ -9,27 +9,40 @@ const Card = () => {
           <Image
             src="/img/download.png"
             alt="octocat"
-            width={111}
-            height={333}
+            width={117}
+            height={117}
             className={styles.image_cover}
           />
         </div>
         <div className={styles.main_data}>
-        <div className={styles.main_top}>
-          <h2 className={styles.card_title}>The Octocat</h2>
-          <h4>22/12/1998</h4>
-          <h3>@loh</h3>
+          <div className={styles.main_top}>
+            <h2 className={styles.card_title}>The Octocat</h2>
+            <h4 className={styles.card_date}>Joined 22 Dec 1998</h4>
+            <h3 className={styles.card_link}>@loh</h3>
+          </div>
+          <div className={styles.bio}>
+            <p className={styles.card_p}>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+              odio. Quisque volutpat mattis eros.
+            </p>
+          </div>
+
+          <ul className={styles.list_main}>
+            <li>
+              <h4>Repos</h4>
+              <h2>8</h2>
+            </li>
+            <li>
+              <h4>Followers</h4>
+              <h2>1488</h2>
+            </li>
+            <li>
+              <h4>Following</h4>
+              <h2>12</h2>
+            </li>
+          </ul>
         </div>
-        <p className={styles.card_p}>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
-            odio. Quisque volutpat mattis eros.
-          </p>
-
-        </div>
-
-
       </div>
-      
     </div>
   );
 };
